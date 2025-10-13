32cb4fc64928b78d764dc3c413de5352d09dc1a2
End <105844010+Endeprey1247@users.noreply.github.com>
2025-10-13 16:07:01 +0700

    Update soal 8.lua

--- soal 8.lua
+++ soal 8.lua
@@ -1,14 +1,9 @@
-local mikir = {-8, -3, -1, 2, 5, 9}
-
---[[function reverseSort(a, b)
-	return a > b
-end]]
-
+-- soal 8
 local function hitung_volatilitas(array)
 	local hasil = {}
 	local awal = 1
 	local akhir = #array
-
+	
 	for i = 1, #array do
 		if array[awal] ^ 2 < array[akhir] ^ 2 then
 			table.insert(hasil, array[akhir] ^ 2)
@@ -18,9 +13,8 @@ local function hitung_volatilitas(array)
 			awal = awal + 1
 		end
 	end
-
+	
 	return hasil
 end
 
-local result = hitung_volatilitas(mikir)
-print(result)
+print(hitung_volatilitas({-8, -3, -1, 2, 5, 9}))
