4c1cd268c246614bd6564666aec20e7ddfa82287
End <105844010+Endeprey1247@users.noreply.github.com>
2025-10-13 11:30:29 +0700

    Create soal 3.lua

---
+++ soal 3.lua
@@ -0,0 +1,8 @@
+-- soal 3
+local lagu = string.gsub("burung kaka tua, hinggap dijendela,", "%w+", {burung="biring", kaka="kiki", tua="tii", hinggap="hinggip", dijendela="dijindili"})
+local lagu2 = string.gsub("nenek sudah tua, giginya tinggal dua", "%w+", {nenek="nunuk", sudah="suduh", tua="tuu", giginya="gugunyu", tinggal="tunggul", dua="duu"})
+
+
+print(lagu, lagu2)
+
+-- do note that this is SUCKASS I HATE IT GRAAAAH;OISADSAOIJFSDFO;SKFMKFDUIOSJFSJDF;SDOFSDNIHSBSONBIJOISNJOIBS
