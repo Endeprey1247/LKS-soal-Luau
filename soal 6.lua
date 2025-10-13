e40e139c72fc86993a01edc94ff5c4751fe7be81
End <105844010+Endeprey1247@users.noreply.github.com>
2025-10-13 11:36:03 +0700

    Create soal 6.lua

---
+++ soal 6.lua
@@ -0,0 +1,30 @@
+--[[game.Players.PlayerAdded:Connect(function(plr)
+	plr.CharacterAdded:Connect(function(char)
+		local bodyColor = char:WaitForChild("BodyColors")
+		if bodyColor.BrickColor == "Medium Brown" then
+			plr:Kick()
+		end
+	end)
+end)]]
+
+--soal 6
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Stack = require(ReplicatedStorage:WaitForChild("Stack"))
+
+local s = Stack.new()
+
+s:push("A")
+
+s:push("B")
+
+s:push("C")
+
+print(s:pop())
+
+print(s:pop())
+
+s:push("E")
+
+print(s:pop())
+
+print(s:pop())
